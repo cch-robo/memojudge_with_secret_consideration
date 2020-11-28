130b8c671e899570c0f10e0cc08620929c699f6b
cch-robo <robo@cch-lab.com>
2020-11-28 18:10:53 +0900

    プラットフォームチェックを追加（コマンドのコメントアウト化を解除）

--- build_assists/scripts/encode_openssl_work_to_public.sh
+++ build_assists/scripts/encode_openssl_work_to_public.sh
@@ -74,7 +74,8 @@ then
 		mkdir ../encode_public
 	fi
 
-  # opensslエンコード  openssl enc -aes-256-cbc -e -pbkdf2 -iter 100000 -base64 -salt -in ../work_private/"$1" -out ../encode_public/"$2" -pass pass:$3
+  # opensslエンコード
+  openssl enc -aes-256-cbc -e -pbkdf2 -iter 100000 -base64 -salt -in ../work_private/"$1" -out ../encode_public/"$2" -pass pass:$3
 	if [ ! -e ../encode_public/"$2" ]
 	then
     echo ""
